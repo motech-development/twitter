8f6acdc4873ae85475587ec4135342623002b7dd
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2021-09-21 13:57:05 +0100

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.eslint.json", "./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "sort-keys": [
+      "error",
+      "asc",
+      {
+        caseSensitive: true,
+        natural: false,
+      },
+    ],
+  },
+};
